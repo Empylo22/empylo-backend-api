d0be6b40cd3308eac8f235214f515a3aea6bb3c7
Hassan Ismail <email>
2024-04-20 12:26:00 +0100

    IP removed

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .setTitle('Employ Project')
     .setDescription('The Employ API description.')
     .setVersion('1.0')
-    .addServer(`http://${ipAddress}:${httpPort}`)
+    // .addServer(`http://${ipAddress}:${httpPort}`)
     .addBearerAuth({
       in: 'header',
       type: 'http',
